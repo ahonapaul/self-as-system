872e67a29c867f9ab576e24c2bc6ddfde7279477
Ahona Paul <paulahona03@gmail.com>
2023-10-12 13:56:49 -0400

    final push

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ var pagename;
 function openPage(pagename){
     window.location.assign(pagename);
 }
+
+function changeBg(){
+    document.body.style.backgroundColor = "white";
+}
+
+function restoreBg(){
+    document.body.style.backgroundColor = "black";
+}
